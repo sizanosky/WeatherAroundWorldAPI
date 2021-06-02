9c21d58e51e08882e028100be2fa33440a38d171
Marcos Fabricio Sizanosky <fabricio_sizanosky@hotmail.com>
2021-06-02 04:10:40 -0300

    Criadas as funções kelvin_para_celsius e valida_saida

--- helpers.py
+++ helpers.py
@@ -3,7 +3,42 @@ def cabecalho():
     Função para formatar um cabeçalho para o programa.
     :return: string
     """
-    texto = "Weather around World - API by OpenWeather"
+    texto = "Weather around World - Using API by OpenWeather"
+    print(f'{"=" * len(texto) + 10 * "="}')
     print(f'{"*" * len(texto) + 10 * "*"}')
     print(f"++++ {texto} ++++")
     print(f'{"*" * len(texto) + 10 * "*"}')
+    print(f'{"=" * len(texto) + 10 * "="}')
+
+
+def kelvin_para_celsius(temperatura_kelvin):
+    """
+    - Função para converter temperatura de Kelvin para Celsius.
+    - Formula: (Kelvin) - 273.15
+    :param temperatura_kelvin: Entrada, temperatura em Kelvin.
+    :return: Retorna a temperatura em Celsius.
+    """
+    temperatura_celsius = temperatura_kelvin - 273.15
+    return temperatura_celsius
+
+
+def valida_saida():
+    """
+    Função para verificar se o usuário deseja continuar executando
+    o programa.
+    :return: Entrada do usuário.
+    """
+
+    while True:
+        continuar = input("\nDeseja realizar outra pesquisa? [S/N]: ")
+        if continuar.lower() == 's':
+            break
+
+        elif continuar.lower() == 'n':
+            print("Encerrando o programa...")
+            print("Obrigado, até a próxima!!!")
+            exit(0)
+
+        else:
+            print('OOOPS! Digite "S" para SIM ou "N" para NÃO.')
+            continue
--- main.py
+++ main.py
@@ -10,31 +10,42 @@ import requests
 from helpers import *
 from config import *
 
-cabecalho()
-print("\nAcesse dados meteorológicos atuais para qualquer local na Terra, \n"
-      "incluindo mais de 200.000 cidades! meteorológicas. ")
+while True:
+    cabecalho()
+    print("\nAcesse dados meteorológicos atuais para qualquer local na Terra, \n"
+          "incluindo mais de 200.000 cidades! Processando dados meteorológicos \n"
+          "de diferentes fontes, como modelos meteorológicos globais e locais, \n"
+          "satélites, radares e uma vasta rede de estações meteorológicas. ")
 
-nome_cidade = input("\nDigite o nome da cidade: ")
+    nome_cidade = input("\nDigite o nome da cidade: ")
 
-# api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-url_completa = f"{URL_BASE}q={nome_cidade}&appid={API_KEY}"
-print(url_completa)
+    # api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+    url_completa = f"{URL_BASE}q={nome_cidade}&appid={API_KEY}"
+    #  print(url_completa)
 
-dados_recebidos = requests.get(url_completa).json()
-print(dados_recebidos)
+    dados_recebidos = requests.get(url_completa).json()
+    #  print(dados_recebidos)
 
-if dados_recebidos['cod'] != '404':
-      #  Dados chave main
+    if dados_recebidos['cod'] != '404':
+        principal = dados_recebidos['main']
+        temp_atual = principal['temp']
+        pressao_atual = principal['pressure']
+        humidade_atual = principal['humidity']
+        visibilidade_atual = dados_recebidos['visibility']
 
-      principal = dados_recebidos['main']
-      temp_atual = principal['temp']
-      pressao_atual = principal['pressure']
-      humidade_atual = principal['humidity']
+        # Dados chave "weather"
+        clima = dados_recebidos['weather']
+        descricao_clima = clima[0]['description']
 
-      # Dados chave "weather"
-      clima = dados_recebidos['weather']
-      descricao_clima = clima[0]['description']
+        #  Mostrar os seguintes valores
+        print(f"\n# Cidade..................: {nome_cidade.title()}")
+        print(f"# Temperatura.............: {round(kelvin_para_celsius(temp_atual), 1)} Cº")
+        print(f"# Umidade do ar...........: {humidade_atual}%")
+        print(f"# Pressão atmosférica.....: {pressao_atual} hPa")
+        print(f"# Visibilidade agora......: {visibilidade_atual / 1000} Km")
+        print(f"# Descrição clima.........: {descricao_clima}")
 
-else:
-      print("Erro!!! Cidade não encontrada!")
+    else:
+        print("Erro >>> Cidade não encontrada!")
 
+    valida_saida()
